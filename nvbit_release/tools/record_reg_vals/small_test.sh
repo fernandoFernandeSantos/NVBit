576484036c088abd71a4a0e634815da64da6900d
Fernando Fernandes <fernandofernandesantos@gmail.com>
2021-07-10 21:18:15 -0300

    Update on small test

---
+++ nvbit_release/tools/record_reg_vals/small_test.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -x
+set -e
+
+SAMPLES_DIR=$HOME/NVIDIA_CUDA-11.3_Samples/0_Simple/matrixMul
+RECORD_REGS_DIR=$HOME/git_research/NVBit/nvbit_release/tools/record_reg_vals
+
+# Build the instrumenter
+cd $RECORD_REGS_DIR
+make clean
+make -j3
+cd -
+
+# go to samples
+cd $SAMPLES_DIR
+make clean
+make -j4
+
+
+eval eval LD_PRELOAD=$RECORD_REGS_DIR/record_reg_vals.so ./matrixMul -wB=64 -wA=64 -hB=64 -hA=64
+
+
+exit 0
